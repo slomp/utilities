1209f00f4fa9ef7611719a7bbd9d77449ea1a9fe
Marcos Slomp <mslomp@gmail.com>
2019-02-14 11:42:12 -0800

    updating nim-cmd.bat such that it can call nim-env.bat from any working directory

--- nim/nim-cmd.bat
+++ nim/nim-cmd.bat
@@ -1,6 +1,2 @@
-:: Copyright (C) 2019 Marcos Slomp - All Rights Reserved
-:: You may use, distribute and modify this code under the
-:: terms of the MIT license.
-
-@CALL nim-env.bat
+@CALL %~dp0\\nim-env.bat
 nim %*
